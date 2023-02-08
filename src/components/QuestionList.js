77a1784ad8b6e29c278842c8afd5caa2c2e47fc2
GezMin <s.shvetsov.v@gmail.com>
2023-02-08 19:03:30 +0600

    Quiz

---
+++ src/components/QuestionList.js
@@ -0,0 +1,40 @@
+export const QuestionList = [
+    {
+        text: 'What is the capital of France?',
+        answers: [
+            { text: 'Paris', correct: true },
+            { text: 'Berlin', correct: false },
+            { text: 'London', correct: false },
+            { text: 'Madrid', correct: false },
+        ],
+        hint: 'It is one of the most famous cities in the world',
+    },
+    {
+        text: 'Какой язык учить',
+        answers: [
+            { text: 'React', correct: true },
+            { text: 'Angular 8', correct: false },
+            { text: 'Vie', correct: false },
+        ],
+        hint: 'такой же какой и я',
+    },
+
+    {
+        text: 'какую базу использовать',
+        answers: [
+            { text: 'MySQL', correct: false },
+            { text: 'MongoDB', correct: true },
+            { text: 'Oraqle', correct: false },
+        ],
+        hint: 'даже не знаю',
+    },
+    {
+        text: 'что такое Sass',
+        answers: [
+            { text: 'компилятор', correct: true },
+            { text: 'style', correct: false },
+            { text: 'Redux', correct: false },
+        ],
+        hint: 'препроцесор',
+    },
+];
---
+++ src/components/Quiz.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { QuestionList } from './QuestionList';
+
+const Quiz = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [answers, setAnswers] = useState([]);
+    const [showHint, setShowHint] = useState(false);
+
+    const handleAnswerClick = index => {
+        setAnswers([...answers, index]);
+        setCurrentQuestion(currentQuestion + 1);
+    };
+
+    const handleHintClick = () => {
+        setShowHint(true);
+    };
+
+    const handleSkipClick = () => {
+        setCurrentQuestion(currentQuestion + 1);
+    };
+
+    const renderQuestion = () => {
+        const question = QuestionList[currentQuestion];
+
+        return (
+            <>
+                <p className='question'>{question.text}</p>
+                <div className='answers'>
+                    {question.answers.map((answer, index) => (
+                        <button
+                            key={answer.text}
+                            className={answers.includes(index) ? 'active' : ''}
+                            onClick={() => handleAnswerClick(index)}
+                        >
+                            {answer.text}
+                        </button>
+                    ))}
+                </div>
+                {showHint && <p className='hint'>{question.hint}</p>}
+                <button onClick={handleHintClick}>Show hint</button>
+                <button onClick={handleSkipClick}>Skip</button>
+            </>
+        );
+    };
+
+    const renderResults = () => {
+        let correctAnswers = 0;
+        QuestionList.forEach((question, index) => {
+            if (question.answers[answers[index]].correct) {
+                correctAnswers += 1;
+            }
+        });
+
+        return (
+            <>
+                <p>
+                    You got {correctAnswers} out of {QuestionList.length} questions right
+                </p>
+            </>
+        );
+    };
+
+    return (
+        <div className='quiz'>
+            {currentQuestion < QuestionList.length ? renderQuestion() : renderResults()}
+        </div>
+    );
+};
+
+export default Quiz;
